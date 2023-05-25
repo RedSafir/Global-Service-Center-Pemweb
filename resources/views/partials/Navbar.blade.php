73f0f421e72afdd673b4bfb665d08ccdb19dac67
Rizki-Herdian <mrhbird.herdian@gmail.com>
2023-05-25 19:53:01 +0700

    Revisi-UI

--- resources/views/partials/Navbar.blade.php
+++ resources/views/partials/Navbar.blade.php
@@ -1,12 +1,19 @@
 <style>
-    /* .navbar {
+    .navbar {
       position: fixed;
       top: 0;
       left: 0;
       right: 0;
       z-index: 9999;
       justify-content: center;
-    } */
+    
+        background-color: #343A40;
+        border-bottom: 4px solid #FFC107;
+    
+    } 
+    body {
+            background-color: #AAAAAA;
+        }
 
     .navbar-brand {
       margin-right: auto;
@@ -16,13 +23,13 @@
 
 </style>
 <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-          <a class="navbar-brand" href="Main">Global Service Center</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-        </div>
+  <nav class="navbar navbar-dark bg-dark fixed-top">
+    <div class="container-fluid">
+        
+        <a class="navbar-brand text-center mx-auto" href="/home">
+            <img src="{{ asset('images\image1.jpg') }}" alt="Logo" width="40" height="40" class="me-2">
+            Global Service Center
+        </a>
+    </div>
       </nav>
 </header>
--- resources/views/partials/NavbarHome.blade.php
+++ resources/views/partials/NavbarHome.blade.php
@@ -1,93 +1,104 @@
 <style>
+     body {
+            background-color: #AAAAAA;
+        }
     .navbar {
-            background-color: #343A40;
-        }
+        background-color: #343A40;
+        border-bottom: 4px solid #FFC107;
+    }
 
-        .navbar-brand {
-            color: #FFFFFF;
-        }
+    .navbar-brand {
+        color: #FFFFFF;
+        font-weight: bold;
+        font-size: 20px;
+    }
 
-        .navbar-toggler {
-            border-color: #FFFFFF;
-        }
+    .navbar-toggler {
+        border-color: #FFFFFF;
+    }
 
-        .offcanvas-header {
-            background-color: #343A40;
-            color: #FFFFFF;
-        }
+    .offcanvas-header {
+        background-color: #343A40;
+        color: #FFFFFF;
+        border-bottom: 1px solid #FFFFFF;
+    }
 
-        .offcanvas-body {
-            background-color: #AAAAAA;
-            color: #FFFFFF;
-        }
+    .offcanvas-body {
+        background-color: #343A40;
+        color: #FFFFFF;
+        border-right: 4px solid #FFC107;
+    }
 
-        .list-group-item {
-            background-color: #AAAAAA;
-            color: #FFFFFF;
-            border-color: #343A40;
-        }
+    .list-group-item {
+        background-color: #AAAAAA;
+        color: #FFFFFF;
+        border-color: #343A40;
+    }
+    .table {
+    background-color: #343A40;
+    color: #FFFFFF;
+}
+    .btn-primary {
+        background-color: #007BFF;
+        border-color: #FFFFFF;
+    }
+    .btn-warning {
+        background-color: #FFC107;
+        border-color: #343A40;
+    }
+    .btn-danger {
+        background-color: #DC3545;
+        border-color: #FFFFFF;
+    }
 
-        .list-group-item:hover {
-            background-color: #343A40;
-        }
+.table tbody tr:hover {
+    background-color: rgba(52, 58, 64, 0.8);
+}
+    .list-group-item:hover {
+        background-color: #222222;
+    }
+    a {
+        color: #FFFFFF;
+    }
 </style>
 <body class="fix-header">
-    <!-- ============================================================== -->
-    <!-- Preloader -->
-    <!-- ============================================================== -->
-
-
-
-    <!-- ============================================================== -->
-    <!-- Wrapper -->
-    <!-- ============================================================== -->
-
-    <!-- ============================================================== -->
-    <!-- Topbar header - style you can find in pages.scss -->
-    <!-- ============================================================== -->
-
-    <!-- End Top Navigation -->
-    <!-- ============================================================== -->
-    <!-- Left Sidebar - style you can find in sidebar.scss  -->
-    <!-- ============================================================== -->
     <nav class="navbar navbar-dark bg-dark fixed-top">
         <div class="container-fluid">
             <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <a class="navbar-brand text-center mx-auto" href="/home">Global Service Center </a>
+            <a class="navbar-brand text-center mx-auto" href="/home">
+                <img src="{{ asset('images\image1.jpg') }}" alt="Logo" width="40" height="40" class="me-2">
+                Global Service Center
+            </a>
             <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
                 <div class="offcanvas-header">
                     <h2 class="offcanvas-title" id="profileOffcanvasLabel">
-                        <img src="{{ asset('images\image1.jpg') }}" alt="Logo Profil" width="40" height="40">
-
+                        <img src="{{ asset('images\image1.jpg') }}" alt="Logo Profil" width="40" height="40" class="me-2">
+                        Admin
                     </h2>
                     <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
                 <div class="offcanvas-body">
-                    <ul class="list-group">
+                    <ul class="list-group mt-3">
                         <li class="list-group-item"><a href="/home">Dashboard</a></li>
+                    </ul>
+                    <ul class="list-group mt-3">
                         <li class="list-group-item"><a href="/Stock">Stock</a></li>
+                    </ul>
+                    <ul class="list-group mt-3">
                         <li class="list-group-item"><a href="/Pelanggan">Pelanggan</a></li>
+                    </ul>
+                    <ul class="list-group mt-3">
                         <li class="list-group-item"><a href="/Transaksi">Transaksi</a></li>
                     </ul>
-                    <div class="mt-3">
+                    <div class="mt-5">
                         <ul class="list-group">
-
                             <li class="list-group-item"><a href="/logout">Logout</a></li>
-
                         </ul>
-
                     </div>
                 </div>
             </div>
         </div>
     </nav>
-
-    <!-- ============================================================== -->
-    <!-- End Left Sidebar -->
-    <!-- ============================================================== -->
-    <!-- ============================================================== -->
-    <!-- Page Content -->
-    <!-- ============================================================== -->
 </body>
